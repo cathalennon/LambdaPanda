52eeaa7e625cc7bbc204763ca73453ea23a3ea2a
user <user@gmail.com>
2015-04-02 11:26:44 +1300

    added router file and enter key to start

---
+++ router.js
@@ -0,0 +1,9 @@
+document.Ready(function(e) {
+	document.addEventListener("keypress", startGame(e));
+})
+
+function startGame(e) {
+	if(e.keyCode === 13) {
+		//use the controller function here to start game
+	}
+}
