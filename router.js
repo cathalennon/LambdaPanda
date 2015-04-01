f06a136f7cd69ac9e967f2883b8f78c721dd962e
user <user@gmail.com>
2015-04-02 12:02:26 +1300

    lunch time

--- router.js
+++ router.js
@@ -1,9 +1,10 @@
-document.Ready(function(e) {
-	document.addEventListener("keypress", startGame(e));
-})
-
+$(document).ready(function(){
+	$(document).keypress(startGame(e));
+});
 function startGame(e) {
 	if(e.keyCode === 13) {
+		console.log("You can start");
 		//use the controller function here to start game
 	}
 }
+ready(startGame());
