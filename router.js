9f5b675c5449f6408b27a8c0693e8c817f18b7a5
user <user@gmail.com>
2015-04-02 15:13:18 +1300

    keypress working and html skeleton

--- router.js
+++ router.js
@@ -1,10 +1,12 @@
-$(document).ready(function(){
-	$(document).keypress(startGame(e));
+$(document).ready(function(e){
+	$(document).keyup(function(event){
+	startGame(event);
+});
 });
 function startGame(e) {
-	if(e.keyCode === 13) {
-		console.log("You can start");
+	 var code = e.keyCode || e.which;
+	if(code == 13) {
+		$('#gamebox p').text("You can start!!!");
 		//use the controller function here to start game
 	}
 }
-ready(startGame());
