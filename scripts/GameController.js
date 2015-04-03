0c0a71215296aeb38b3c99f4c4186f04ac9fa776
Ash <mitchell.mcewen@users.noreply.github.com>
2015-04-03 16:14:55 +1300

    Added sound

--- scripts/GameController.js
+++ scripts/GameController.js
@@ -7,17 +7,21 @@ GameController.prototype.startGame = function() {
 	this.gameView.renderWelcome();
 	this.startChase();
 	this.startTimer();
+	$('.soundTheme').trigger('play');
+	$('.soundId').trigger('play');
+
 };
 
 GameController.prototype.startTimer = function() {
 	setInterval(function(){
             this.game.elapsedTime += 0.1;
-            this.gameView.renderTimer(this.game);
+            this.gameView.renderTimer(this.game);          
             if (Math.random() * 5 < 1){
-            this.spawner.createFollower();
-            
+            this.spawner.createFollower();                     
             }
+
       }.bind(this), 100);
+	
 };
 
 GameController.prototype.saveCursor = function(x,y) {
--- scripts/Spawner.js
+++ scripts/Spawner.js
@@ -6,5 +6,7 @@ function Spawner(gameView){
 Spawner.prototype.createFollower = function(){
 	var follower = new Follower('../Assets/test.png');
 	this.gameView.renderFollower(follower.element);
-
+	if (Math.random() * 10 < 1){
+	//$('.soundId').trigger('play');
+}
 };
